d090728627b31638b72091c3f8c0fc2e8f6a4bad
adityapawar112 <adityapawar112358@gmail.com>
2024-07-25 14:24:41 +0530

    user suggestions implemented

--- socialpidia/core/views.py
+++ socialpidia/core/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
+import random
 
 
 #Created a view for the main page
@@ -12,6 +13,8 @@ from itertools import chain
 def index(request):
     user_object = User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user = user_object)
+
+    #user feed(only shows posts by user and users that the user is following)
 
     user_following_list = []
     feed = []
@@ -33,7 +36,33 @@ def index(request):
     feed_list = list(chain(*feed))  
 
     posts = sorted(feed_list, key=lambda x: x.created_at, reverse=True) #adds all the posts
-    return render(request, 'index.html', {'user_profile' : user_profile, 'posts' : feed_list,})
+
+    #user suggestions
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username = user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username = request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for user in final_suggestions_list:
+        username_profile.append(user.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+    
+    return render(request, 'index.html', {'user_profile' : user_profile, 'posts' : feed_list, 'suggestions_username_profile_list' : suggestions_username_profile_list[:5]})
 
 @login_required(login_url='signin')
 def like_post(request):
